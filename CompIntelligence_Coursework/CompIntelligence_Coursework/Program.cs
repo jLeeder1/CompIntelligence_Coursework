d7c28054469bf41cfa9facec3ee0d71ec1dfb802
Josh Leeder <joshleeder1@gmail.com>
2020-11-07 14:59:34 +0000

    First commit

---
+++ CompIntelligence_Coursework/CompIntelligence_Coursework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompIntelligence_Coursework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
