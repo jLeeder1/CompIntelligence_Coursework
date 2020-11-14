c80411d7ee7b4f7a82af70b3f90b788b25e3ce46
Josh Leeder <joshleeder1@gmail.com>
2020-11-14 17:03:37 +0000

    Tests added

---
+++ CompIntelligence_Coursework/CompIntelligence_Coursework_TestSuite/Helpers/SolutionStrategyFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using CompIntelligence_Coursework.Helpers;
+using CompIntelligence_Coursework.Models;
+using CompIntelligence_Coursework.PSO;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace CompIntelligence_Coursework_TestSuite.Helpers
+{
+    public class SolutionStrategyFactoryTests
+    {
+        private ISolutionStrategyFactory solutionStrategyFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            Mock<IPieceLengthToQuantityLookup> mockPieceLengthToQuantityLookup = new Mock<IPieceLengthToQuantityLookup>();
+            Mock<IStockLengthToCostLookup> mockstockLengthToCostLookup = new Mock<IStockLengthToCostLookup>();
+
+            solutionStrategyFactory = new SolutionStrategyFactory(mockPieceLengthToQuantityLookup.Object, mockstockLengthToCostLookup.Object);
+        }
+
+        [Test]
+        public void GetSolutionFinderStrategy_ShouldReturn_PSOSolution_WhenStrategyTypeIs_PSOSolutionStrategy()
+        {
+            // Arrange
+            var strategyType = SolutionStrategyTypes.PSOSolutionStrategy;
+
+            // Act
+            var result = solutionStrategyFactory.GetSolutionFinderStrategy(strategyType);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(PSOSolution));
+        }
+    }
+}
