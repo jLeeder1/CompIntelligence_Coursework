8e7ad67118b0ca4669e54a8fbf18bb9e78b8de5e
Josh Leeder <joshleeder1@gmail.com>
2020-11-15 18:38:19 +0000

    Parent selection implemented no tests yet

---
+++ CompIntelligence_Coursework/CompIntelligence_Coursework/EvolutionaryAlgorithm/IParentSelection.cs
@@ -0,0 +1,10 @@
+﻿using CompIntelligence_Coursework.Models;
+using System.Collections.Generic;
+
+namespace CompIntelligence_Coursework.RandomGenerator
+{
+    public interface IParentSelection
+    {
+        List<Solution> GetParentPopulation(List<Solution> currentParentPopulation);
+    }
+}
