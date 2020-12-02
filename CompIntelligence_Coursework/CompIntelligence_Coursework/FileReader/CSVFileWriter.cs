3ba22defe3d7a1a18453dd5ad79d71b90a409aa6
leeder.josh <leederjl@aston.ac.uk>
2020-12-02 19:52:18 +0000

    File writer complete

--- CompIntelligence_Coursework/CompIntelligence_Coursework/FileReader/CSVFileWriter.cs
+++ CompIntelligence_Coursework/CompIntelligence_Coursework/FileReader/CSVFileWriter.cs
@@ -1,4 +1,6 @@
-﻿using CompIntelligence_Coursework.Models;
+﻿using CompIntelligence_Coursework.EvolutionaryAlgorithm;
+using CompIntelligence_Coursework.Generic;
+using CompIntelligence_Coursework.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,6 +19,12 @@ namespace CompIntelligence_Coursework.FileReader
             // Write the string array to a new file named "WriteLines.txt".
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "TestResults.txt")))
             {
+                outputFile.WriteLine($"Is using adaptive crossover,{EvolutionaryAlgorithmConstants.IS_USING_ADAPTIVE_CROSSOVER_CHANCES}");
+                outputFile.WriteLine($"Number of generations,{GenericConstants.NUMBER_OF_ITERATIONS}");
+                outputFile.WriteLine($"Number of iterations in generation,{GenericConstants.NUMBER_OF_ITERATIONS}");
+                outputFile.WriteLine($"Tournament size,{EvolutionaryAlgorithmConstants.TOURNAMENT_SIZE}");
+                outputFile.WriteLine($"Number of tests,{GenericConstants.NUMBER_OF_TEST_RESULTS_TO_CREATE}");
+                outputFile.WriteLine("Generation cost found in" + ","+ "Best cost value");
                 foreach (Result result in results)
                     outputFile.WriteLine($"{result.GenerationResultFoundIn},{result.SolutionCost}");
             }
